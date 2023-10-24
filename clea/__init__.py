6566b3fbc168aa31c895e0f7e6480dc1c4e03e0d
angrybayblade <vptl185@gmail.com>
2023-10-24 15:01:00 +0530

    feat: add support for top-level imports

--- clea/__init__.py
+++ clea/__init__.py
@@ -1,3 +1,20 @@
 """
-A lightweight clea for writing CLI tools in python.
+A lightweight framework for writing CLI tools in python.
 """
+
+from .context import Context
+from .params import (
+    Boolean,
+    Choice,
+    ChoiceByFlag,
+    ContextParameter,
+    Directory,
+    File,
+    Float,
+    Integer,
+    String,
+    StringList,
+    VersionParameter,
+)
+from .runner import run
+from .wrappers import command, group
