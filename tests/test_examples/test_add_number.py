8494ae30d661f246d5c59118d8df5395dd456a7a
angrybayblade <vptl185@gmail.com>
2023-10-30 19:12:43 +0530

    test: add_numbers.py coverage

---
+++ tests/test_examples/test_add_number.py
@@ -0,0 +1,21 @@
+"""Test add.py"""
+
+from examples.add_numbers import add as cli
+from clea.runner import run
+
+
+def test_missing_arguments() -> None:
+    """Test add."""
+    result = run(cli=cli, argv=[], isolated=True)
+    assert result.exit_code == 1
+    assert (
+        "Missing argument for positional arguments <N1 type=int>, <N2 type=int>"
+        in result.stderr
+    )
+
+
+def test_add() -> None:
+    """Test add."""
+    result = run(cli=cli, argv=["1", "2"], isolated=True)
+    assert result.exit_code == 0
+    assert "Total 3" in result.stdout
